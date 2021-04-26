9178b7405ae4b30c0504ff7e9115022ff0a6b632
pichkasik <sasha.marakulin@mail.ru>
2021-04-26 14:44:50 +0300

    Refactor code

--- philo_one/src/lifecycle/philo_lifecycle.c
+++ philo_one/src/lifecycle/philo_lifecycle.c
@@ -12,13 +12,21 @@
 
 #include "philo_one.h"
 
+void			print_message(struct timeval time, t_philosopher *philo, char *message)
+{
+	long int curr_time;
+
+	gettimeofday(&time, NULL);
+	curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
+	printf("I am a philo - %d | %s - %ld\n", philo->number_of_philo, message, curr_time - philo->start_time);
+}
+
 void			*is_philosopher_death(void *arg)
 {
 	int i;
 	t_philosopher *arr_philo;
 	struct timeval time;
 	long int curr_time;
-	long int		start_time;
 
 	arr_philo = arg;
 	i = 0;
@@ -27,8 +35,6 @@ void			*is_philosopher_death(void *arg)
 		wait_philo_sit_to_table(&arr_philo[i]);
 		i++;
 	}
-	gettimeofday(&time, NULL);
-	start_time = time.tv_sec * 1000 + time.tv_usec / 1000;
 	while (1)
 	{
 		i = 0;
@@ -36,17 +42,14 @@ void			*is_philosopher_death(void *arg)
 		{
 			gettimeofday(&time, NULL);
 			curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
-//			printf("I am a philo - %d | time_stand_ms - %ld\n", arr_philo[i].number_of_philo, curr_time - start_time);
 			if (curr_time - arr_philo[i].last_meal >= arr_philo->param->time_to_die && arr_philo[i].last_meal != 0)
 			{
 				arr_philo[i].state = DIED;
 				printf("Philo - %d DIED\n", arr_philo[i].number_of_philo);
-				break;
+				return (NULL);
 			}
 			i++;
 		}
-		if (arr_philo[i].state == DIED)
-			break;
 	}
 	return (NULL);
 }
@@ -67,17 +70,12 @@ void			*philo_lifecycle(void *arg)
 		get_fork(philo);
 		gettimeofday(&time, NULL);
 		philo->last_meal = time.tv_sec * 1000 + time.tv_usec / 1000;
-		curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
-		printf("I am a philo - %d | EATING - %ld\n", philo->number_of_philo, curr_time - philo->start_time);
+		print_message(time, philo, "EATING");
 		my_usleep(philo->param->time_to_eat);
 		put_fork(philo);
-		gettimeofday(&time, NULL);
-		curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
-		printf("I am a philo - %d | SLEEPING - %ld\n", philo->number_of_philo, curr_time - philo->start_time);
+		print_message(time, philo, "SLEEPING");
 		my_usleep(philo->param->time_to_sleep);
-		gettimeofday(&time, NULL);
-		curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
-		printf("I am a philo - %d | THINKING - %ld\n", philo->number_of_philo, curr_time - philo->start_time);
+		print_message(time, philo, "THINKING");
 	}
 	return (NULL);
 }
