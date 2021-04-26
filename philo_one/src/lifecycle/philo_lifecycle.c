a63e29fb8e8a9e0d62f40ce226f23b088c6119fc
pichkasik <sasha.marakulin@mail.ru>
2021-04-26 15:24:32 +0300

    refactor

--- philo_one/src/lifecycle/philo_lifecycle.c
+++ philo_one/src/lifecycle/philo_lifecycle.c
@@ -21,6 +21,7 @@ void			*is_philosopher_death(void *arg)
 {
 	int i;
 	t_philosopher *arr_philo;
+	int main_condition;
 
 	arr_philo = arg;
 	i = 0;
@@ -34,12 +35,14 @@ void			*is_philosopher_death(void *arg)
 		i = 0;
 		while (arr_philo->param->nbr_philosophers != i)
 		{
-			if (get_current_time() - arr_philo[i].last_meal >= arr_philo->param->time_to_die && arr_philo[i].last_meal != 0)
+			main_condition = get_current_time() - arr_philo[i].last_meal >= arr_philo->param->time_to_die && arr_philo[i].last_meal != 0;
+			if (main_condition)
 			{
 				arr_philo[i].state = DIED;
 				printf("Philo - %d DIED\n", arr_philo[i].index_philo);
-				printf("curtime = %ld | last_meal = %ld | time_to_die = %ld\n" ,
-					   get_current_time(), arr_philo[i].last_meal, arr_philo->param->time_to_die);
+				//sometimes here just die philo if total number of philo is 4 or pair (wierd)
+				printf("curtime = %ld | last_meal = %ld | time_to_die = %ld\n" , get_current_time(), arr_philo[i].last_meal, arr_philo->param->time_to_die);
+				printf("result of condition - %d\n", get_current_time() - arr_philo[i].last_meal >= arr_philo->param->time_to_die && arr_philo[i].last_meal != 0);
 				return (NULL);
 			}
 			i++;
@@ -51,16 +54,14 @@ void			*is_philosopher_death(void *arg)
 void			*philo_lifecycle(void *arg)
 {
 	t_philosopher	*philo;
-//	struct timeval	time;
 
 	philo = arg;
 	wait_philo_sit_to_table(philo);
-	philo->last_meal = get_current_time();
-	philo->start_time = philo->last_meal;
+	philo->start_time = get_current_time();
+	philo->last_meal = philo->start_time;
 	while (1)
 	{
 		get_fork(philo);
-//		gettimeofday(&time, NULL);
 		philo->last_meal = get_current_time();//time.tv_sec * 1000 + time.tv_usec / 1000;
 		print_message(philo, "EATING");
 		my_usleep(philo->param->time_to_eat);
