7de5f28a91babf40deb9d9ea49509c9bb2381362
pichkasik <sasha.marakulin@mail.ru>
2021-05-09 19:05:15 +0300

    refactor

--- philo_two/main.c
+++ philo_two/main.c
@@ -21,7 +21,7 @@ int	main(int argc, char *argv[])
 	self = ft_calloc(1, sizeof(t_self));
 	self->param = get_parameters(argc, argv);
 	self->semaphore = init_semaphore(self->param);
-	self->arr_philo = init_philosophers(self->param, self->semaphore);
+	self->arr_philo = init_philosophers(self->param);
 	create_threads(self);
 	clear_all_data(self);
 	printf("End of simulation\n");
